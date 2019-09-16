2dd2c3b77e5ce4952639acc9455d6332bc7030d8
Stven Lin <857119585@qq.com>
2019-09-16 13:47:32 +0800

    initial

---
+++ class_my_vector/main.cpp
@@ -0,0 +1,106 @@
+//class vec 向量类
+#include <iostream>
+using namespace std;
+
+const int maxDigit = 10;
+
+class vec
+{
+    friend istream & operator >> (istream & is, vec & obj);
+    friend ostream & operator << (ostream & os, const vec & obj);
+private:
+    int group[maxDigit], digit;
+public:
+    //空构造函数（存在bug）
+    vec()
+    {
+        //group = {0};
+        digit = 1;
+    }
+    //构造函数
+    vec(int grp[], int dgt)
+    {
+        digit = dgt;
+        for (int i = 0; i < dgt; ++i)
+        {
+            group[i] = grp[i];
+        }
+    }
+    //拷贝构造函数
+    vec(const vec & obj)
+    {
+        digit = obj.digit;
+        for (int i = 0; i < obj.digit; ++i)
+        {
+            group[i] = obj.group[i];
+        }
+    }
+    //加法重载
+    vec operator + (const vec & rightV)
+    {
+        vec result = rightV; //调用了一次拷贝构造函数
+        for (int i = 0; i < digit; ++i)
+        {
+            result.group[i] = group[i] + rightV.group[i];
+        }
+        return result;
+    }
+    //减法重载
+    vec operator - (const vec & rightV)
+    {
+        vec result = rightV; //调用了一次拷贝构造函数
+        for (int i = 0; i < digit; ++i)
+        {
+            result.group[i] = group[i] - rightV.group[i];
+        }
+        return result;
+    }
+    //内积重载
+    int operator * (const vec & rightV)
+    {
+        int result = 0;
+        for (int i = 0; i < digit; ++i)
+        {
+            result += group[i] * rightV.group[i];
+        }
+        return result;
+    }
+};
+
+//输入重载，【输入方案不好，无法按需输入】
+istream & operator >> (istream & is, vec & obj)
+{
+    bool flag = true; char flag2;
+    for (int i = 0; flag; ++i)
+    {
+        obj.group[i] = is.get() - 48;
+        flag2 = is.get();
+        if (flag2 == '\n') flag = false;
+    }
+    return is;
+}
+//输出重载
+ostream & operator << (ostream & os, const vec & obj)
+{
+    os << '(';
+    for (int i = 0; i < obj.digit; ++i) os << obj.group[i] << ',';
+    os << ')';
+    return os;
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    int g1[] = {1, 2, 3}, g2[] = {4, 5, 6};
+    vec v1(g1, 3), v2(g2, 3),
+        v3(g2, 3);  //输入对应的对象，输入的向量维数须在这里指定（bug）
+    cout << v1 << v2 << endl;
+    cout << "v1 + v2 : " << v1 + v2 << endl;
+    cout << "v1 - v2 : " << v1 - v2 << endl;
+    cout << "v1 * v2 : " << v1 * v2 << endl;
+    cout << "input vec（单个空格分开数字，最后一个数字后直接回车，维数只能为3）: " << endl;
+    cin >> v3;
+    cout << "v3' : " << v3 << endl;
+    cout << "v1 + v3 : " << v1 + v3 << endl;
+    return 0;
+}
