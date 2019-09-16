e3ef4d439d10e199d1040b466c89f1ea0628ec30
Stven Lin <857119585@qq.com>
2019-09-16 23:15:40 +0800

    update counter_cpp

---
+++ counter/main.cpp
@@ -0,0 +1,51 @@
+/*
+ 1. 对报警计数器函数进行修改，要求：
+ 可对输入时设定的报警值（非负）进行报警。计数器从0开始计数，到达报警值时发出警告。
+ 如：
+ Warning value：5
+ 1
+ 2
+ 3
+ 4
+ Warning：5！
+*/
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+void delay(int sec)
+{
+    time_t start_time, cur_time; // 变量声明
+    time(&start_time);
+    do { time(&cur_time);
+    } while((cur_time - start_time) < sec );
+}
+
+int main()
+{
+    int number = 0;
+    cout << "please enter a integer: ";
+    cin >> number;
+    
+    if (
+        (cin.get() != '\n' and
+         cin.get() != '\t' and
+         cin.get() != ' ') and
+         number >= 0
+        )
+    {
+        cout << "Error!" << endl;
+        return -1;
+    }
+    else
+    {
+        for (int i = 1; i <= number; ++i)
+        {
+            delay(1);
+            if (i == number) cout << "Warning: " << number << " !" << endl;
+            else cout << i << endl;
+        }
+        return 0;
+    }
+}
